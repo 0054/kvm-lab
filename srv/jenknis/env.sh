e65debf6266497165291ce779e2523b0ab954aa4
0054 <kofe54@gmail.com>
2019-11-22 15:15:23 +0300

    jenkins api script

---
+++ srv/jenknis/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export JENKINS_API_TOKEN=11696ee854346774b111f3265498ea8b00                                                                                                                                                 
+export JENKINS_USER=admin
+export JENKINS_SERVER=10.10.1.12
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "vm_config" {
   default = {
     # "inf-registry" = { vcpu = 1, memory = 512, ip = ["10.10.1.11"], disk = 10 }
-    # "inf-jenkins" = { vcpu = 2, memory = 2048, ip = ["10.10.1.12"], disk = 10 }
-    "web-01"   = { vcpu = 1, memory = 512, ip = ["10.10.1.13"], disk = 10 }
-    "web-02"   = { vcpu = 1, memory = 512, ip = ["10.10.1.14"], disk = 10 }
-    "haproxy1" = { vcpu = 1, memory = 512, ip = ["10.10.1.15"], disk = 10 }
-    "haproxy2" = { vcpu = 1, memory = 512, ip = ["10.10.1.16"], disk = 10 }
-    # "centos-vm4" = { vcpu = 1, memory = 512, ip = ["10.10.1.14"], disk = 10 }
-    # "centos-vm5" = { vcpu = 1, memory = 512, ip = ["10.10.1.15"], disk = 10 }
+    "inf-jenkins" = { vcpu = 2, memory = 2048, ip = ["10.10.1.12"], disk = 10 }
+    # "web-01" = { vcpu = 1, memory = 512, ip = ["10.10.1.13"], disk = 10 }
+    # "web-02" = { vcpu = 1, memory = 512, ip = ["10.10.1.14"], disk = 10 }
+    # "haproxy1" = { vcpu = 1, memory = 512, ip = ["10.10.1.15"], disk = 10 }
+    # "haproxy2" = { vcpu = 1, memory = 512, ip = ["10.10.1.16"], disk = 10 }
+    # "lb-nginx1" = { vcpu = 1, memory = 512, ip = ["10.10.1.17"], disk = 10 }
+    # "lb-nginx2" = { vcpu = 1, memory = 512, ip = ["10.10.1.18"], disk = 10 }
   }
 }
 
